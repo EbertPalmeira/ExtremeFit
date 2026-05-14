04203e8b7c936e9b4cc1e9487e02d3fb79fced77
Ebert Palmeira <eberttpalmerass@gmail.com>
2026-05-14 14:13:19 -0300

    adicionando autenticacoes na classe usuario

---
+++ src/main/java/extreme/fit/domain/usuario/Usuario.java
@@ -0,0 +1,28 @@
+package extreme.fit.domain.usuario;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "usuario")
+@Entity(name = "Usuario")
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+
+    private String senha;
+
+
+}
--- src/main/java/extreme/fit/infra/exception/TratadordeErros.java
+++ src/main/java/extreme/fit/infra/exception/TratadordeErros.java
@@ -1,4 +1,4 @@
-package extreme.fit.infra;
+package extreme.fit.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
