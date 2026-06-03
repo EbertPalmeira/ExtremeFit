68ebdd6f83c33c55aee5cd204b091d9e3970e451
Ebert Palmeira <eberttpalmerass@gmail.com>
2026-06-03 15:34:14 -0300

    terminando testes em professorController

--- src/test/java/extreme/fit/controller/ProfessorControllerTest.java
+++ src/test/java/extreme/fit/controller/ProfessorControllerTest.java
@@ -11,7 +11,8 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 
 
 @SpringBootTest
@@ -40,4 +41,63 @@ class ProfessorControllerTest {
         assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
     }
 
+    @Test
+    @WithMockUser
+    void deveriaDevolverCodigo200AoListarProfessores() throws Exception {
+        var response = mvc.perform(
+                get("/professor")
+        ).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+
+    }
+
+    @Test
+    @WithMockUser
+    void deveriaDevolverCodigo200AoAtualizarProfessor() throws Exception {
+        String json = """
+                {
+                        "id":1,
+                        "nome": "lucas",
+                        "email": "lucas@123.com",
+                        "Especialidade": "MUSCULACAO"
+                }
+                """;
+
+
+        var response = mvc.perform(
+                put("/professor")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+
+    }
+    @Test
+    @WithMockUser
+    void deveriaDevolverCodigo204AoExcluirProfessor() throws Exception {
+        String json = """
+                {
+                        "id":"1",
+                        "nome": "ebert",
+                        "email": "ebert@123.com",
+                        "cref": "476764",
+                        "Especialidade": "HIPERTROFIA"
+                }
+                """;
+
+
+        var response = mvc.perform(
+                delete("/professor/{id}",1)
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NO_CONTENT.value());
+
+
+    }
 }
